554864c8cc0cf5f8ecb9d93dda898e1d8c494a10
Roman <roman.nieronov@mail.ru>
2024-06-23 18:35:29 +0300

    Update lock for new req. Update aiogram bot init for new ver.

--- bot/core/loader.py
+++ bot/core/loader.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
 from aiogram.utils.i18n.core import I18n
@@ -11,7 +12,7 @@ app = web.Application()
 
 token = settings.BOT_TOKEN
 
-bot = Bot(token=token, parse_mode=ParseMode.HTML)
+bot = Bot(token=token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 redis_client = Redis(
     connection_pool=ConnectionPool(
